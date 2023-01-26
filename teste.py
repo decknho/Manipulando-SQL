6a23d14cfe361106935227c05e9b2a074b246987
Dereck Eder <decknho26@gmail.com>
2023-01-26 17:20:54 -0300

    formatando uma tabela
    para visualizar todas as informações da tabela de forma organizada

--- teste.py
+++ teste.py
@@ -8,6 +8,8 @@ conexao = pymysql.connect(
 
 cursor = conexao.cursor()
 cursor.execute("select * from gafanhotos")
-
+print(f'\033[35m{"ID":<3}| {"Nome":<30}| {"Sexo":<10}| {"Profissão":<25}| {"Nascimento"}  | {"Peso":<8}| {"Altura":<8}|'
+      f' {"Nacionalidade":>3}\033[m')
+print('-'*123)
 for x in cursor:
-    print(x)
+    print(f'{x[0]:<3}| {x[1]:<30}| {x[4]:<10}| {x[2]:<25}| {x[3]}  | {x[5]:<8}| {x[6]:<8}| {x[7]:>3}')
